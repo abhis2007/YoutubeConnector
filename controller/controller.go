04f6400110708e06511e87d71ec80c2170fddc99
Abhishek Kumar <abhishek.kumarg@microfocus.com>
2024-01-30 00:23:51 +0530

    Added the functionality for updating the video by their snippet data/metadata.

--- controller/controller.go
+++ controller/controller.go
@@ -10,6 +10,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/abhis2007/YOUTUECONNECTOR/config"
 )
@@ -45,14 +46,81 @@ func UploadVideo(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type snippetBody struct {
+	Title       string `json:"title"`
+	CategoryId  string `json:"categoryId"`
+	Description string `json:"description"`
+}
+
+type mainBody struct {
+	Id      string      `json:"id"`
+	Snippet snippetBody `json:"snippet"`
+}
+
+func updateVideo(snippet string, videoId string) {
+	fmt.Println(videoId)
+	accessToken := config.OAUTH_TOKEN_KR8799
+	uploadEndpoint := "https://youtube.googleapis.com/youtube/v3/videos?part=snippet"
+
+	fmt.Println(uploadEndpoint)
+
+	snippetParam := snippetBody{
+		Title:       "title_new_struct",
+		CategoryId:  "22",
+		Description: "NeeDesc",
+	}
+	bodyParams := mainBody{
+		Id:      videoId,
+		Snippet: snippetParam,
+	}
+	jsonData, err := json.Marshal(bodyParams)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+	
+	fmt.Println(string(jsonData))
+	
+
+	//Create the request
+	request, err := http.NewRequest("PUT", uploadEndpoint, strings.NewReader(string(jsonData)))
+
+	if err != nil {
+		return
+	}
+
+	// Add the YouTube API key or OAuth 2.0 access token to the request
+	request.Header.Set("Authorization", "Bearer "+accessToken)
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	//return
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("Error making request: %v\n", err)
+		return
+	}
+	fmt.Println("full response:", response)
+	defer response.Body.Close()
+
+	// Read and print the response
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %v\n", err)
+		return
+	}
+
+	fmt.Println("Response:", string(responseBody))
+
+}
+
 func callUpload(snippet string) {
 	fmt.Println("callupload")
-	// title := "My Video Title"
-	// description := "Description of my video."
-	// privacyStatus := "public" // Set to "public" for a public video
+	fmt.Println(snippet)
 
 	// YouTube API endpoint
 	apiEndpoint := config.ROOT_URL + "/" + "upload/youtube/v3/videos"
+	//apiEndpoint += "?part=snippet,status"
 
 	// Path to the video file to upload
 	videoFilePath := "C:\\Users\\AKumar22\\Downloads\\testvd.mp4"
@@ -63,13 +131,22 @@ func callUpload(snippet string) {
 	// Create a new multipart request
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
+	var fw io.Writer
 
-	// Add video metadata as form fields
-	// writer.WriteField("snippet", fmt.Sprintf(`{"title":"%s","description":"%s","privacyStatus":"%s"}`, title, description, privacyStatus))
-
-	writer.WriteField("snippet", snippet)
-	fmt.Println(snippet)
-	// return
+	//adding the non-video data
+	fw, err := writer.CreateFormField("snippet")
+	if err != nil {
+		fmt.Println("error while writinf the data : ", err)
+		return
+	}
+	// _, err = io.Copy(fw, strings.NewReader(snippet))
+	_, err = io.Copy(fw, bytes.NewReader([]byte(snippet)))
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println("Snippet data copied.")
+	}
 
 	// Add the video file
 	file, err := os.Open(videoFilePath)
@@ -79,7 +156,7 @@ func callUpload(snippet string) {
 	}
 	defer file.Close()
 
-	part, err := writer.CreateFormFile("videoFile", file.Name())
+	part, err := writer.CreateFormFile("file", file.Name())
 	if err != nil {
 		fmt.Printf("Error creating form file: %v\n", err)
 		return
@@ -89,6 +166,8 @@ func callUpload(snippet string) {
 	if err != nil {
 		fmt.Printf("Error copying file content: %v\n", err)
 		return
+	} else {
+		fmt.Println("File data copied.")
 	}
 
 	// Close the multipart writer
@@ -100,13 +179,12 @@ func callUpload(snippet string) {
 		fmt.Printf("Error creating request: %v\n", err)
 		return
 	}
-
 	// Set the content type header for multipart requests
 	request.Header.Set("Content-Type", writer.FormDataContentType())
 
 	// Add the YouTube API key or OAuth 2.0 access token to the request
 	request.Header.Set("Authorization", "Bearer "+apiKeyOrAccessToken)
-
+	//return
 	// Perform the request
 	client := http.Client{}
 	response, err := client.Do(request)
@@ -127,14 +205,54 @@ func callUpload(snippet string) {
 	fmt.Println("Response:", string(responseBody))
 }
 
-// type Snippet struct {
-// 	title       string   `json:"title"`
-// 	description string   `json:"description"`
-// 	tags        []string `json:"TagInput"`
-// 	categoryId  string   `json:"Category"`
-// }
-
 func FetchAndUploadVideo(w http.ResponseWriter, r *http.Request) {
+
+	type VideoSnippet struct {
+		Title       string   `json:"title"`
+		Description string   `json:"description"`
+		CategoryId  string   `json:"categoryId"`
+		Tags        []string `json:"tags,omitempty"`
+	}
+	type VideoStatus struct {
+		PrivacyStatus string `json:"privacyStatus"`
+	}
+	type Video struct {
+		Snippet *VideoSnippet `json:"snippet"`
+		Status  *VideoStatus  `json:"status"`
+	}
+
+	title := "Sample Title"
+	description := "Sample Description"
+	category := "24"
+	privacy := "unlisted"
+
+	// Create Video instance
+	upload := &Video{
+		Snippet: &VideoSnippet{
+			Title:       title,
+			Description: description,
+			CategoryId:  category,
+			Tags:        []string{"tag1", "tag2"},
+		},
+		Status: &VideoStatus{
+			PrivacyStatus: privacy,
+		},
+	}
+
+	// Marshal the struct into JSON
+	jsonData, err := json.Marshal(upload)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	// Print the resulting JSON string
+	//fmt.Println(string(jsonData))
+	//callUpload(string(jsonData))
+	updateVideo(string(jsonData), "zHA-YusgSSo")
+}
+
+func FetchAndUploadVideos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	type formValue struct {
 		Title           string   `json:"title"`
@@ -145,8 +263,16 @@ func FetchAndUploadVideo(w http.ResponseWriter, r *http.Request) {
 		TagInput        []string `json:"tagInput"`
 		Privacy         string   `json:"privacy"`
 	}
+	// type snippetData struct {
+	// 	Snippet formValue `json:snippet`
+	// }
 	var d formValue
+
 	json.NewDecoder(r.Body).Decode(&d)
+
+	// data := snippetData {
+	// 	Snippet: d,
+	// }
 	fmt.Println(d.Title)
 	fmt.Println(d.Description)
 	// fmt.Println(d.Category)
@@ -172,8 +298,9 @@ func FetchAndUploadVideo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	snippet := fmt.Sprintf(`{"title":"%s","description":"%s","tags":%s, "categoryId":"%s"}`, d.Title, d.Description, tagsJSON, "22")
-	fmt.Println(snippet)
-	callUpload(snippet)
+	data := fmt.Sprintf(`{"snippet" : %s}`, snippet)
+	//fmt.Println(snippet)
+	callUpload(data)
 	// fmt.Println(snippet)
 }
 
